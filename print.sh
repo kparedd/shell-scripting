83fdba7cb97c18e089e6e5b5aee650d6d1566b98
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-14 20:37:07 -0400

    data dynamics commands

--- print.sh
+++ print.sh
@@ -3,9 +3,12 @@
 A=abhikp
 echo a=$A
 Date=2021-07-14
-echo hey bro you know the $Date today
+echo hey bro you know today is $Date
+
 #commands
 NO_OF_USERS=$(ls)
 echo number of users=$NO_OF_USERS
 
-#
+#Date with command substitution
+Date=$(date +%F)
+echo hey bro you know today is $Date
