e38c066abae35c934611898c702fef4dce200e00
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-15 19:23:27 -0400

    declare and call the functions

--- function.sh
+++ function.sh
@@ -4,9 +4,12 @@
 sample(){
   echo welcome i am sample function
   echo cost of A drink = ${a}
+  echo first agrument in function = $1
 }
 
 #main program
 #call the command
 a=100
 sample
+echo first agrument in main program = $1
+
