764b176f88fe743236c178ea0529b20114f898a2
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-16 19:40:19 -0400

    outputs from command should not be displayed

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
+
 echo -e "installing nginx\t\t ...\t\t\e[32mdone\e[0m"
-yum install nginx -y
+yum install nginx -y >/tmp/roboshop.log
 
 echo "enabling nginx"
-systemctl enable nginx
+systemctl enable nginx >/tmp/roboshop.log
 
 echo "starting nginx"
-systemctl start nginx
+systemctl start nginx >/tmp/roboshop.log
