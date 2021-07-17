85b32d8d8f39a88cf13326a57297e49a8bcb5325
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-16 20:58:19 -0400

    exit commands

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,8 +4,14 @@ LOG=/tmp/roboshop.log
 
 rm -f $LOG
 
-echo -e "installing nginx\t\t ...\t\t\e[32mdone\e[0m"
+echo -e "installing nginx\t\ .."
 yum install nginx -y &>>$LOG
+if [ $? -eq 0]; then
+  echo -e "\e[32m done\e[0m"
+else
+  echo -e "\e[31m fail\e[0m"
+fi
+
 
 echo "enabling nginx"
 systemctl enable nginx &>>$LOG
