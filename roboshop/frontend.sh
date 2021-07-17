fd0a0783de06794d80d1fa2ed3c5150a9ac7c8c8
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-16 21:05:25 -0400

    validating  whether scripting is working or not

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,7 +6,7 @@ rm -f $LOG
 
 echo -e "installing nginx\t\ .."
 yum install nginx -y &>>$LOG
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
 else
   echo -e "\e[31m fail\e[0m"
@@ -15,14 +15,14 @@ fi
 
 echo -n -e "enabling nginx\t\t"
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
 else
   echo -e "\e[31m fail\e[0m"
 fi
 echo -n -e "starting nginx\t\t"
 systemctl start nginx &>>$LOG
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
 else
   echo -e "\e[31m fail\e[0m"
