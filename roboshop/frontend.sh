c964d707fa4ba71332e263f2543495c4ed8fe045
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-19 12:25:08 -0400

    IMPORTING THE SOURCE

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,18 +3,7 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
-STAT_CHECK() {
-  if [ $1 -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[31m fail\e[0m"
-  exit 1
-fi
-}
-
-PRINT() {
-  echo -n -e "$1\t\t.."
-}
+source common.sh
 
 PRINT "installing nginx"
 yum install nginx -y &>>$LOG
