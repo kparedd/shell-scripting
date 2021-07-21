99983067be258de33e22d507ea038fbcbe4211d6
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-21 19:32:36 -0400

    redis

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -6,11 +6,11 @@ PRINT " Install redis repos"
 yum install epel-release yum-utilshttp://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Install Redis "
-yum install redis -y --enablerepo &>>$LOG
+PRINT "Install Redis"
+yum install redis -y --enablerrepo=remi &>>$LOG
 STAT_CHECK $?
 
-PRINT "Update Redis"
+PRINT "Update Redis listen address"
 echo -i -e '/s/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>$LOG
 STAT_CHECK $?
 
