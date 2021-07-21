d6646594a654988b3c93bd838c055afe86710c51
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-21 15:50:55 -0400

    catalogue

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -2,7 +2,7 @@
 
 source common.sh
 
-PRINT "Install Nodejs"
+PRINT "Install Nodejs\t"
 yum install nodejs make gcc-c++ -y &>>$LOG
 STAT_CHECK $?
 
@@ -13,14 +13,17 @@ if [ $? -ne 0 ]; then
 fi
 STAT_CHECK $?
 
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/catalogue.zip
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
-$ npm install
-NOTE: We need to update the IP address of MONGODB Server in systemd.service file. Replace MONGO_DNSNAME with IP address.
-Now, lets set up the service with systemctl.
+PRINT "Download Application Code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT " Extract Downloaded Code"
+cd /home/roboshop && unzip /tmp/catalogue.zip && rm -rf catalogue && mv catalogue-main catalogue &>>$LOG
+STAT_CHECK $?
+
+PRINT "Install NodeJS Dependencies"
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
+
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
