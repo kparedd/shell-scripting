6e5f52cf34915cfedab8579a70ce0c2155210e44
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-21 11:49:37 -0400

    MONGODB automation

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/bash
+
+source common.sh
+
+PRINT "Setting the Repository"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-Install Mongo & Start Service.
-# yum install -y mongodb-org
+STAT_CHECK $?
+
+PRINT "Installing Mongo"
+yum install -y mongodb-org &>>$LOG
+STAT_CHECK $?
+
 # systemctl enable mongod
 # systemctl start mongod
 Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
