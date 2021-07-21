1c751aca4814f0385a65036d6544238a98433231
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-21 11:32:54 -0400

    frontend automation

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,6 +6,22 @@ PRINT "installing nginx"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
+PRINT "Download Frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove Old HtDocs"
+cd /usr/share/nginx/html && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>LOG
+STAT_CHECK $?
+
+PRINT "Update Roboshop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT_CHECK $?
+
 PRINT "Enabling nginx\t"
 systemctl enable nginx &>>$LOG
 STAT_CHECK $?
