6fc6b6d21652e4112e68c290183bff22e4e85eba
kparedd <83795719+kparedd@users.noreply.github.com>
2021-07-21 21:02:39 -0400

    user

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -24,38 +24,40 @@ PRINT() {
   echo -n -e "$1\t\t.."
 }
 
-PRINT "Install Nodejs\t\t"
-yum install nodejs make gcc-c++ -y &>>$LOG
-STAT_CHECK $?
+NODEJS {
+  PRINT "Install Nodejs\t\t"
+  yum install nodejs make gcc-c++ -y &>>$LOG
+  STAT_CHECK $?
 
-PRINT "Add RoboShop Application user"
-id roboshop &>>$LOG
-if [ $? -ne 0 ]; then
-  useradd roboshop &>>$LOG
-fi
-STAT_CHECK $?
+  PRINT "Add RoboShop Application user"
+  id roboshop &>>$LOG
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>$LOG
+  fi
+  STAT_CHECK $?
 
-PRINT "Download Application Code"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
-STAT_CHECK $?
+  PRINT "Download Application Code"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+  STAT_CHECK $?
 
-PRINT " Extract Downloaded Code"
-cd /home/roboshop && unzip /tmp/${COMPONENT}.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue &>>$LOG
-STAT_CHECK $?
+  PRINT " Extract Downloaded Code"
+  cd /home/roboshop && unzip /tmp/${COMPONENT}.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue &>>$LOG
+  STAT_CHECK $?
 
-PRINT "Install NodeJS Dependencies"
-cd /home/roboshop/${COMPONENT} && npm install --unsafe-perm &>>$LOG
-STAT_CHECK $?
+  PRINT "Install NodeJS Dependencies"
+  cd /home/roboshop/${COMPONENT} && npm install --unsafe-perm &>>$LOG
+  STAT_CHECK $?
 
-PRINT "Fix application user\t"
-chown roboshop:roboshop /home/roboshop -R &>>$LOG
-STAT_CHECK $?
+  PRINT "Fix application user\t"
+  chown roboshop:roboshop /home/roboshop -R &>>$LOG
+  STAT_CHECK $?
 
-PRINT "Setup SystemD file\t"
-sed -i -e sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/"
-/home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-STAT_CHECK $?
+  PRINT "Setup SystemD file\t"
+  sed -i -e sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/"
+  /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  STAT_CHECK $?
 
-PRINT "Start ${COMPONENT} Service\t"
-systemctl daemon-reload &>>$LOG && systemctl start ${COMPONENT} &>>$LOG  && systemctl enable ${COMPONENT} &>>$LOG
-STAT_CHECK $?
+  PRINT "Start ${COMPONENT} Service\t"
+  systemctl daemon-reload &>>$LOG && systemctl start ${COMPONENT} &>>$LOG  && systemctl enable ${COMPONENT} &>>$LOG
+  STAT_CHECK $?
+}
